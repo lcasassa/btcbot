022b7ee320ee91620a42e84b5fdf59e077a8ba6e
Linus Casassa <linus@surbtc.com>
2018-03-01 04:08:50 -0300

    adding date and removing non transactions

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ while True:
         p = p_bbc
 
         if v > 0.0005:
-            print >> sys.stderr, '++++++ v =', v, 'p =', p*100.0, 'total = ', r['total']
+            print >> sys.stderr, '++++++ v =', v, 'p =', p*100.0, 'total = ', r['total'], datetime.now()
             sys.stderr.flush()
 
             buda.oc(v, b['ask']['btc'])
@@ -78,7 +78,7 @@ while True:
         v = min(bc['bid']['vol'], b['ask']['vol'], 0.001, sbc['cop']/bc['ask']['btc'])
         p = p_bcb
         if v > 0.0005:
-            print >> sys.stderr, '------ v =', v, 'p =', p*100.0, 'total = ', r['total']
+            print >> sys.stderr, '------ v =', v, 'p =', p*100.0, 'total = ', r['total'], datetime.now()
             sys.stderr.flush()
 
             buda_cop.oc(v, bc['ask']['btc'])
@@ -90,7 +90,7 @@ while True:
         r['v'] = 0
         r['p'] = 0
 
-        print >> sys.stderr, '       v =', r['v'], 'p =', r['p']*100.0, 'total = ', r['total']
+        #print >> sys.stderr, '       v =', r['v'], 'p =', r['p']*100.0, 'total = ', r['total']
         sys.stderr.flush()
 
     print(json.dumps(r))
